8f62bdde10fd01fdf54d34ef9f608c6ec4c69a2f
anuarharb <anuarharb@hotmail.com>
2018-03-05 17:56:27 -0600

    google page ready

--- google/assets/javascript/scripts.js
+++ google/assets/javascript/scripts.js
@@ -1,8 +1,12 @@
 function search(form) {
-  var topic = form.searchInput.value;
+  var topic = form.topic.value;
   location.assign('https://www.google.com.mx/search?q=' + topic);
 }
 
-document.querySelector('#search-input').addEventListener('change', function() {
-  console.log('cambio');
+document.querySelector('#topic').addEventListener('keypress', function keyupText(e) {
+  var key = e.which || e.keyCode;
+  if (key === 13) {
+    e.preventDefault();
+    search(document.querySelector('form'));
+  }
 })
