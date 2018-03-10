048767f058abd113f79fb179dc3f71ee00cc0481
anuarharb <anuarharb@hotmail.com>
2018-03-09 20:49:43 -0600

    animatioins and transitions

---
+++ html-y-css/animaciones/scripts.js
@@ -0,0 +1,43 @@
+var background = document.querySelector('body');
+var goku = document.querySelector('.goku');
+var gokuSSJ = document.querySelector('.goku-ssj');
+var picture = document.querySelector('picture');
+
+function superSajayin() {
+  goku.style.display = "none";
+  background.classList.add('flash');
+  gokuSSJ.style.display = "block";
+}
+
+function normalSajayin() {
+  gokuSSJ.style.display = "none";
+  background.classList.remove('flash');
+  goku.style.display = "block";
+}
+
+function moveLeft() {
+  picture.style.left = "15%";
+  picture.style.width = "250px";
+  goku.src = "img/goku-fly.png";
+  gokuSSJ.src = "img/goku-ssj-fly.png";
+}
+
+function moveCenter() {
+  picture.style.left = "42%";
+}
+
+function moveRight() {
+  picture.style.left = "70%";
+  picture.style.transform = "scaleX(-1)";
+  picture.style.transform = "rotate(100deg)";
+  picture.style.width = "225px";
+  goku.src = "img/goku-fly.png";
+  gokuSSJ.src = "img/goku-ssj-fly.png";
+}
+
+picture.addEventListener("transitionend", function(event) {
+  goku.src = "img/goku2.png"
+  gokuSSJ.src = "img/goku-ssj.png";
+  picture.style.width = "150px";
+  picture.style.transform = "scaleX(1)";
+}, false);
