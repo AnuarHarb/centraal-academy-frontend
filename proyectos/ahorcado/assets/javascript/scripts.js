c129308b6344049cb27e7f7a2cbdd7a0b628e490
Anuar Harb <anuarharb@hotmail.com>
2018-04-04 22:02:34 -0500

    error handler para manejar errores

--- proyectos/ahorcado/assets/javascript/scripts.js
+++ proyectos/ahorcado/assets/javascript/scripts.js
@@ -1,6 +1,12 @@
 'use strict';
 var word = ['H','O','L','A','O'];
 
+// Estado del juego
+var state = {
+  'aciertos': 0,
+  'errors': 0
+}
+
 // Maneja el teclado de botones (En pantalla)
 function compareLetter(letter) {
   var selectedLetter = letter.value;
@@ -15,22 +21,31 @@ document.addEventListener('keydown', function getKeyLetter(event) {
 
 // Toma la letra seleccionada y la compara con las letras de la palabra para decidir si fue una jugada correcta o un error
 function playLetter(selectedLetter) {
+  var errorsText = document.querySelector('.errors');
+  var aciertosText = document.querySelector('.aciertos');
   var isMistake = true;
   for (var i = 0; i < word.length; i++) {
     if (selectedLetter == word[i]) {
-      alert('le atinaste a la letra ' + i);
       isMistake = false;
       showLetter(i, selectedLetter);
     }
   }
+  // Resvisa si la bandera isMistake quedo como positiva o negativa para saber is fue un error o no
   if (isMistake == true) {
-    alert('Ahora si estas mal')
+    state.errors = state.errors + 1;
+    errorsText.innerHTML = state.errors;
+    errorHandler();
+  } else {
+    state.aciertos = state.aciertos + 1;
+    aciertosText.innerHTML = state.aciertos;
   }
 }
 
 // Inicia el juego y pinta la palabra para que el jugador sepa de cuantas letras es
 function startGame() {
   var showWord = document.querySelector('.show-word');
+  var keyboard = document.querySelector('.keyboard');
+  keyboard.style.display = 'block';
   for(var i = 0; i < word.length; i++){
     var space = document.createElement('span');
     space.classList.add('letter-box');
@@ -44,4 +59,15 @@ function startGame() {
 function showLetter(position, selectedLetter) {
   var letterBox = document.getElementsByClassName('letter-box');
   letterBox[position].innerHTML = selectedLetter
+}
+
+// Maneja los errores para saber si perdiste o iniciar alguna acción
+function errorHandler() {
+  if(state.errors == 1) {
+    alert('Primer error');
+  } else if(state.errors == 2) {
+    alert('Segundo Error');
+  } else {
+    alert('Perdiste, existe Master Card');
+  }
 }
