1e86b8c44d9287550e621c7d3995e082d9050b1b
Anuar Harb <anuarharb@hotmail.com>
2018-04-13 22:11:28 -0500

    inicio de pagina personal

---
+++ javascript/jqueryProject/javascript/script.js
@@ -0,0 +1,14 @@
+
+// 
+//
+//
+// // // Vanilla Js
+// var titles = document.getElementsByTagName('h1');
+// for(var i = 0; i<= titles.length; i++) {
+//   titles[i].style.color = "red";
+// }
+//
+// // // jQuery
+// $('h1').css('color', 'red');
+//
+// $('h1').css('width', '10%');
---
+++ proyectos/pagina-personal/assets/javascript/scripts.js
@@ -0,0 +1,6 @@
+$('button').on('mousedown', function() {
+  $('button').css('margin-top', '3px');
+})
+$('button').on('mouseup', function() {
+  $('button').css('margin-top', '0');
+})
