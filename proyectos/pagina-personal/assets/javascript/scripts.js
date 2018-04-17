766132d0fbd12c592285430740eba4031adc6729
Anuar Harb <anuarharb@hotmail.com>
2018-04-16 20:46:27 -0500

    modal added

--- proyectos/pagina-personal/assets/javascript/scripts.js
+++ proyectos/pagina-personal/assets/javascript/scripts.js
@@ -1,6 +1,26 @@
+// Button interaction
 $('button').on('mousedown', function() {
   $('button').css('margin-top', '3px');
-})
+});
+
 $('button').on('mouseup', function() {
   $('button').css('margin-top', '0');
-})
+});
+
+// Modal interaction
+// Open Modal
+$('.modal-button').on('click', function(event) {
+  $('.custom-modal').removeClass('not-showing');
+  if($(event.currentTarget).hasClass('skill1-button') == true) {
+    $('.skill2-modal').hide();
+    $('.skill1-modal').show();
+  } else {
+    $('.skill2-modal').show();
+    $('.skill1-modal').hide();
+  }
+});
+
+// Close modal
+$('.modal-background, .close-modal i').on('click', function() {
+  $('.custom-modal').addClass('not-showing');
+});
