ec1c45f5bf122c3e7b3c5396424c82bf4c74358a
Anuar Harb <anuarharb@hotmail.com>
2018-04-17 21:48:43 -0500

    magic box

--- proyectos/pagina-personal/assets/javascript/scripts.js
+++ proyectos/pagina-personal/assets/javascript/scripts.js
@@ -7,16 +7,26 @@ $('button').on('mouseup', function() {
   $('button').css('margin-top', '0');
 });
 
+
+// Smooth scroll to a section
+$('.nav-link').on('click', function(event) {
+  var target = event.currentTarget.getAttribute('data-target');
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, 400)
+});
+
 // Modal interaction
 // Open Modal
 $('.modal-button').on('click', function(event) {
   $('.custom-modal').removeClass('not-showing');
+  $('.custom-modal-card').hide();
   if($(event.currentTarget).hasClass('skill1-button') == true) {
-    $('.skill2-modal').hide();
     $('.skill1-modal').show();
-  } else {
+  } else if ($(event.currentTarget).hasClass('skill2-button') == true){
     $('.skill2-modal').show();
-    $('.skill1-modal').hide();
+  } else {
+    $('.skill3-modal').show();
   }
 });
 
@@ -24,3 +34,15 @@ $('.modal-button').on('click', function(event) {
 $('.modal-background, .close-modal i').on('click', function() {
   $('.custom-modal').addClass('not-showing');
 });
+
+
+//
+$('.show-magic-box').on('mouseover', function(event) {
+  var target = event.currentTarget.getAttribute('data-active-box');
+  $('.' + target).addClass('magic-box-active');
+})
+
+$('.show-magic-box').on('mouseleave', function(event) {
+  var target = event.currentTarget.getAttribute('data-active-box');
+  $('.' + target).removeClass('magic-box-active');
+})
